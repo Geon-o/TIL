2ea2970a5cdeda4cab17c81ee350c62d89d9df66
G <91590599+Geon-o@users.noreply.github.com>
2022-06-16 18:16:41 +0900

    2022 - 06 - 16
    변수 사용

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -1,1 +1,18 @@
-alert("hi!");
+console.log("hello World"); // console에 "hello World" 출력
+
+// 연산 결과값 출력
+console.log(5 + 2);
+console.log(5 / 2);
+console.log(5 * 2);
+
+// 변수사용
+const num = 5;
+const num2 = 2;
+
+console.log(num + num2);
+console.log(num / num2);
+console.log(num * num2);
+
+// 변수에 텍스트 적용
+const Name = "G";
+console.log(Name);
