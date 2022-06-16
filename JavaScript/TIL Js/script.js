395ad6d2307bf7301d7bb9127a744378c1fcd374
G <91590599+Geon-o@users.noreply.github.com>
2022-06-16 18:33:14 +0900

    2022 - 06 - 16

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -6,13 +6,17 @@ console.log(5 / 2);
 console.log(5 * 2);
 
 // 변수사용
-const num = 5;
-const num2 = 2;
+let num = 5;
+let num2 = 2;
 
 console.log(num + num2);
 console.log(num / num2);
 console.log(num * num2);
 
 // 변수에 텍스트 적용
-const Name = "G";
+let Name = "G";
+console.log(Name);
+
+//let의 사용예시
+Name = "J";  // Name변수 값 업데이트
 console.log(Name);
