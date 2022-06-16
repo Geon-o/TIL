4ecbd16171a037472e1334670cb8f3e9c2d0db85
G <91590599+Geon-o@users.noreply.github.com>
2022-06-16 20:20:12 +0900

    Add files via upload

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -1,10 +1,24 @@
-// boolean
-const a = true;
-const b = false;
-const c = null; // 값이 주어지는데 존재하지 않음
-let d;  //값을 주지않음
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thr = "thr";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+// 그룹화(string)
+const Week = mon + tue + wed + thr + fri + sat + sun;
+console.log(Week);
+
+// 데이터 타입 그룹화(Array)
+const Week2 = [mon , tue , wed , thr , fri , sat , sun];
+console.log(Week2);
+
+//변수 할당 없이
+const Week3 = ["mon" , "tue" , "wed" , "thr" , "fri" , "sat"];
+console.log(Week3);
+// console.log(Week3[index]);  데이터 접근 -> "sat" 출력 배열은 0부터 시작
+
+//데이터 추가
+Week3.push("sun");
+console.log(Week3);
