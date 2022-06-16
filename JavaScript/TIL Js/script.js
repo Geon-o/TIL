1fc42c43722329ef4aff15ce32d4299504306684
G <91590599+Geon-o@users.noreply.github.com>
2022-06-16 19:47:19 +0900

    2022 - 06 - 16

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -1,24 +1,10 @@
-console.log("hello World"); // console에 "hello World" 출력
+// boolean
+const a = true;
+const b = false;
+const c = null; // 값이 주어지는데 존재하지 않음
+let d;  //값을 주지않음
 
-// 연산 결과값 출력
-console.log(5 + 2);
-console.log(5 / 2);
-console.log(5 * 2);
-
-// 변수사용
-
-const num = 5;
-
-const num2 = 2;
-
-console.log(num + num2);
-console.log(num / num2);
-console.log(num * num2);
-
-// 변수에 텍스트 적용
-let Name = "G";
-console.log(Name);
-
-//let의 사용예시
-Name = "J";  // Name변수 값 업데이트
-console.log(Name);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
