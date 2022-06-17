0858a3d154670755b55ebd78413ae7e6e8fe07c9
G <91590599+Geon-o@users.noreply.github.com>
2022-06-18 00:43:30 +0900

    if문 조건사용 / AND, OR

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -1,5 +1,6 @@
-// NaN 판별
+// 값 받기
 const age = parseInt(prompt("몇 살임?"));
+// NaN 판별
 console.log(isNaN(age));
 
 // 조건
@@ -9,9 +10,22 @@ console.log(isNaN(age));
 //     // condition 거짓일 때 실행
 // }
 
-//활용 예시1
-if (isNaN(age)) {
-    console.log("숫자를 입력해주세요.");
-} else {
-    console.log("숫자를 입력하였습니다.")
-}
+// 활용 예시1
+// if (isNaN(age)) {
+//     console.log("숫자를 입력해주세요.");
+// } else {
+//     console.log("숫자를 입력하였습니다.")
+// }
+
+// 활용 예시2
+if (isNaN(age) || age < 0) {
+    console.log("(양의)숫자를 입력해주세요.");
+} else if (age < 18) {
+    console.log("미성년자");
+} else if(age >= 18 && age <= 50){
+    console.log("18세부터 50세");
+} else if(age > 50 && age <= 80){
+    console.log("51살부터 80세");
+} else if(age > 80){
+    console.log("81세~")
+} 
