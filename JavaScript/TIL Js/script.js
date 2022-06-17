9cf1f2090d202bea100010930e211666d4903977
G <91590599+Geon-o@users.noreply.github.com>
2022-06-17 19:59:24 +0900

    객체 안 함수, 함수 인자 보내고 받기

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -4,11 +4,34 @@ console.log("Hello name is b");
 console.log("Hello name is c");
 console.log("Hello name is d");
 
-function Hello(){
-    console.log("hello");
+function Hello(name1, age) {
+    console.log("hello " + name1 + "and " + age);
 }
 
 Hello();
-Hello("G");
-Hello("J");
-Hello("O");
+Hello("G", 20);
+Hello("J", 25);
+Hello("O", 27);
+
+function plus(FN, SN) {
+    console.log(FN + SN);
+}
+
+function minus(a, b) {
+    console.log(a - b);
+}
+
+plus(10, 11);
+minus(50, 10);
+
+// function in object
+const profile = {
+    name: "G",
+    say: function (anotherName) {
+        console.log("hello! " + anotherName + " how are you");
+    },
+};
+
+console.log(profile.name);
+profile.say("J");
+profile.say("O");
