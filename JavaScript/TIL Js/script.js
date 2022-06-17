2c1a7170fa2c2b15c39b93bce6aebf9fddb920ca
G <91590599+Geon-o@users.noreply.github.com>
2022-06-18 00:21:57 +0900

    if문 사용

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -1,13 +1,17 @@
-const age = prompt("몇 살임?");
+// NaN 판별
+const age = parseInt(prompt("몇 살임?"));
+console.log(isNaN(age));
 
+// 조건
+// if (condition) {
+//     // condition 참일때 실행
+// } else {
+//     // condition 거짓일 때 실행
+// }
 
-//타입 변경(string -> number)
-console.log(age1,parseInt(age1));
-
-//타입 확인
-console.log(typeof age1);
-console.log(typeof "11", typeof parseInt("11"));
-
-//최종
-const age1 = parseInt(prompt("몇 살임?"));
-console.log(age1);
+//활용 예시1
+if (isNaN(age)) {
+    console.log("숫자를 입력해주세요.");
+} else {
+    console.log("숫자를 입력하였습니다.")
+}
