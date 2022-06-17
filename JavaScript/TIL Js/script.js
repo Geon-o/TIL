2363ec3177d816fdcc3e5aa09caf69c3cc06e304
G <91590599+Geon-o@users.noreply.github.com>
2022-06-18 00:01:22 +0900

    parseInt를 사용하여 데이터 type변환하기

--- JavaScript/TIL Js/script.js
+++ JavaScript/TIL Js/script.js
@@ -1,85 +1,13 @@
+const age = prompt("몇 살임?");
 
-// console에 출력
-const calculator = {
-    add: function (a, b) {
-        console.log("add = " + (a + b));
-    },
-    min: function (a, b) {
-        console.log("min = " + (a - b));
-    },
-    div: function (a, b) {
-        console.log("div = " + (a / b));
-    },
-    mul: function (a, b) {
-        console.log("mul = " + (a * b));
-    },
-    squ: function (a, b) {
-        console.log("squ = " + (a ** b));
-    },
-};
 
-calculator.add(2, 3);
-calculator.min(2, 3);
-calculator.div(2, 3);
-calculator.mul(2, 3);
-calculator.squ(2, 3);
+//타입 변경(string -> number)
+console.log(age1,parseInt(age1));
 
-// alert 사용
+//타입 확인
+console.log(typeof age1);
+console.log(typeof "11", typeof parseInt("11"));
 
-const calculator2 = {
-    add: function (a, b) {
-        alert("add = " + (a + b));
-    },
-    min: function (a, b) {
-        alert("min = " + (a - b));
-    },
-    div: function (a, b) {
-        alert("div = " + (a / b));
-    },
-    mul: function (a, b) {
-        alert("mul = " + (a * b));
-    },
-    squ: function (a, b) {
-        alert("squ = " + (a ** b));
-    },
-};
-
-calculator2.add(2, 3);
-calculator2.min(2, 3);
-calculator2.div(2, 3);
-calculator2.mul(2, 3);
-calculator2.squ(2, 3);
-
-// return 키워드
-const age = 20;
-function calculateKrAge(ageOfForeigner) {
-    return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-console.log(krAge);
-
-const calculator3 = {
-    add: function (a, b) {
-        return a + b;
-    },
-    min: function (a, b) {
-        return a - b;
-    },
-    div: function (a, b) {
-        return a / b;
-    },
-    mul: function (a, b) {
-        return a * b;
-    },
-    squ: function (a, b) {
-        return a ** b;
-    },
-};
-
-const addTmp = calculator3.add(2, 3);
-const minTmp = calculator3.min(addTmp, 3);
-const divTmp = calculator3.div(2, minTmp);
-const mulTmp = calculator3.mul(divTmp, minTmp);
-const squTmp = calculator3.squ(mulTmp, addTmp);
-
+//최종
+const age1 = parseInt(prompt("몇 살임?"));
+console.log(age1);
